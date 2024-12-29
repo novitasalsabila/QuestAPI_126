d0f9aa78e1995d66157f36672c0b4702c6ee2549
novitasalsabila <novnovv10@gmail.com>
2024-12-30 03:42:30 +0700

    Implementasi `AppContainer` untuk menyediakan dependensi aplikasi terkait mahasiswa

--- app/src/main/java/com/example/pertemuan8/repository/MahasiswaContainer.kt
+++ app/src/main/java/com/example/pertemuan8/repository/MahasiswaContainer.kt
@@ -9,3 +9,20 @@ import retrofit2.Retrofit
 interface AppContainer {
     val mahasiswaRepository: MahasiswaRepository
 }
+class MahasiswaContainer: AppContainer {
+    // URL dasar API (mengarah ke server lokal untuk emulator Android)
+    private val baseUrl = "http://10.0.2.2:8000/umyTI" // `10.0.2.2` digunakan untuk mengakses localhost dari emulator Android
+
+    // Konfigurasi JSON serialization untuk mengabaikan properti yang tidak dikenal dalam respons
+    private val json = Json { ignoreUnknownKeys = true }
+
+    // Membuat instance Retrofit dengan konfigurasi converter JSON dan URL dasar
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        // Menambahkan converter untuk menangani data JSON dengan tipe media `application/json`
+        .baseUrl(baseUrl).build()
+    // Menentukan URL dasar untuk permintaan API
+
+    private val mahasiswaService: MahasiswaService by lazy { retrofit.create(MahasiswaService::class.java) }
+    override val mahasiswaRepository: MahasiswaRepository by lazy { NetworkKontakRepository (mahasiswaService) }
+}
