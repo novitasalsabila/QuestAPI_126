380e45ea7f1c30307f082ec8989772e72fcebf9a
novitasalsabila <novnovv10@gmail.com>
2024-12-30 03:38:18 +0700

    Interface untuk menyediakan dependensi aplikasi

--- app/src/main/java/com/example/pertemuan8/repository/MahasiswaContainer.kt
+++ app/src/main/java/com/example/pertemuan8/repository/MahasiswaContainer.kt
@@ -9,14 +9,3 @@ import retrofit2.Retrofit
 interface AppContainer {
     val mahasiswaRepository: MahasiswaRepository
 }
-
-class MahasiswaContainer: AppContainer {
-    private val baseUrl = "http://10.0.2.2:8000/umyTI"
-    private val json = Json { ignoreUnknownKeys = true }
-    private val retrofit: Retrofit = Retrofit.Builder()
-        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
-        .baseUrl(baseUrl).build()
-
-    private val mahasiswaService: MahasiswaService by lazy { retrofit.create(MahasiswaService::class.java) }
-    override val mahasiswaRepository: MahasiswaRepository by lazy { NetworkKontakRepository (mahasiswaService) }
-}
