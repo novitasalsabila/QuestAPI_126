a742b18a64ab33c01c36429b9e93ecc2a9977ab8
novitasalsabila <novnovv10@gmail.com>
2024-12-30 12:46:22 +0700

    Kode di atas mendefinisikan MahasiswaApp, sebuah Composable yang berfungsi sebagai kontainer untuk aplikasi mahasiswa, dengan menggunakan Scaffold dari Jetpack Compose

--- app/src/main/java/com/example/pertemuan8/MahasiswaApp.kt
+++ app/src/main/java/com/example/pertemuan8/MahasiswaApp.kt
@@ -1,5 +1,6 @@
-package com.example.pertemuan8.ui.viewmodel
+package com.example.pertemuan8
 
+import com.example.pertemuan8.ui.navigasi.PengelolaHalaman
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -9,23 +10,26 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import com.example.pertemuan8.ui.navigasi.PengelolaHalaman
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MahasiswaApp(
-    modifier: Modifier = Modifier
-) {
+fun MahasiswaApp(modifier: Modifier = Modifier) {
+
+    // Mengatur perilaku scroll untuk TopAppBar
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     Scaffold(
+
+        // Menambahkan scroll behavior ke Scaffold
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        //topBar = {TopAppBar(scrollBehavior = scrollBehavior)},
-    ){
+        // topBar = { TopAppBar(scrollBehavior = scrollBehavior) }
+    ) {
+        // Area konten utama
         Surface(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(it)
-        ){
+        ) {
+            // Menampilkan halaman dengan navigasi
             PengelolaHalaman()
         }
     }
