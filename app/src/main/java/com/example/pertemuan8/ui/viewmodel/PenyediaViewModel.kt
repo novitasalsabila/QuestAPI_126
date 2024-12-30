e7967ba363105d1ae15693b8597f6028f072ca94
novitasalsabila <novnovv10@gmail.com>
2024-12-30 12:38:26 +0700

    PenyediaViewModel adalah objek yang menyediakan ViewModelFactory untuk membuat ViewModel yang mengakses mahasiswaRepository dari MahasiswaApplications.

--- app/src/main/java/com/example/pertemuan8/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertemuan8/ui/viewmodel/PenyediaViewModel.kt
@@ -1,17 +1,19 @@
 package com.example.pertemuan8.ui.viewmodel
 
+import com.example.pertemuan8.MahasiswaApplications
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import com.example.pertemuan8.MahasiswaApplications
 
-object PenyediaViewModel {
-    val Factory = viewModelFactory {
-        initializer { HomeViewModel(aplikasiKontak().container.mahasiswaRepository) }
-        initializer { InsertViewModel(aplikasiKontak().container.mahasiswaRepository) }
+object PenyediaViewModel{
+    val Factory = viewModelFactory{
+        initializer { HomeViewModel(MahasiswaApplications().container.mahasiswaRepository) }
+        initializer { InsertViewModel(MahasiswaApplications().container.mahasiswaRepository) }
+        initializer { UpdateViewModel(MahasiswaApplications().container.mahasiswaRepository) }
+        initializer { DetailViewModel(MahasiswaApplications().container.mahasiswaRepository) }
     }
-}
 
-fun CreationExtras.aplikasiKontak(): MahasiswaApplications =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
+    fun CreationExtras.MahasiswaApplications(): MahasiswaApplications =
+        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as MahasiswaApplications)
+}
