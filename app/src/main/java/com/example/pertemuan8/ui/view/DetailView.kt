009e7024e538602b5ca29705b7eeb54d73288074
novitasalsabila <novnovv10@gmail.com>
2024-12-30 11:49:43 +0700

    digunakan untuk menampilkan halaman detail mahasiswa

--- app/src/main/java/com/example/pertemuan8/ui/view/DetailView.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/DetailView.kt
@@ -1,185 +1,200 @@
 package com.example.pertemuan8.ui.view
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertemuan8.model.Mahasiswa
+import com.example.pertemuan8.ui.CostumWidget.CoustumeTopAppBar
 import com.example.pertemuan8.ui.navigasi.DestinasiNavigasi
-import com.example.pertemuan8.ui.viewmodel.DetailMahasiswaUiState
+import com.example.pertemuan8.ui.viewmodel.DetailUiState
 import com.example.pertemuan8.ui.viewmodel.DetailViewModel
 import com.example.pertemuan8.ui.viewmodel.PenyediaViewModel
 
+
+
 object DestinasiDetail : DestinasiNavigasi {
-    override val route = "nim_detail"
+    override val route = "detail/{nim}"
     override val titleRes = "Detail Mahasiswa"
-    const val nim = "nim"
-    val routeWithArgs = "$route/{$nim}"
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DetailView(
-    modifier: Modifier = Modifier,
+fun DetailMhsScreen(
+    nim: String,
     navigateBack: () -> Unit,
-    onEditClick: (String) -> Unit,
-    detailViewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
-){
+    navigateEdit: (String) -> Unit,  // Digunakan untuk navigasi ke layar update
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    LaunchedEffect(nim) {
+        viewModel.getMahasiswaById(nim)
+    }
 
     Scaffold(
         modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
-            CostumeTopAppBar(
+            CoustumeTopAppBar(
                 title = DestinasiDetail.titleRes,
                 canNavigateBack = true,
                 scrollBehavior = scrollBehavior,
                 navigateUp = navigateBack
+
             )
         },
         floatingActionButton = {
             FloatingActionButton(
                 onClick = {
-                    val nim = (detailViewModel.detailMahasiswaUiState as? DetailMahasiswaUiState.Success)?.mahasiswa?.nim
-                    if (nim != null) onEditClick(nim)
+                    navigateEdit(nim)  // Mengirim nim saat ini ke fungsi navigasi
                 },
-                shape = MaterialTheme.shapes.medium
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp),
+                contentColor = Color.White,
+                containerColor = MaterialTheme.colorScheme.primary
             ) {
                 Icon(
                     imageVector = Icons.Default.Edit,
-                    contentDescription = "Edit Mahasiswa",
+                    contentDescription = "Edit Mahasiswa"
                 )
             }
         }
     ) { innerPadding ->
-        Box(
+        val detailUiState = viewModel.detailUiState.collectAsState().value
+        DetailBody(
+            detailUiState = detailUiState,
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(innerPadding).offset(y = (-70).dp)
-        ) {
-            DetailStatus(
-                mhsUiState = detailViewModel.detailMahasiswaUiState,
-                retryAction = { detailViewModel.getMahasiswabyId() },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .align(Alignment.Center)
-                    .padding(16.dp)
-            )
-        }
+                .padding(innerPadding)
+                .fillMaxSize(),
+            onDeleteClick = {
+                viewModel.deleteMhs(nim)
+                navigateBack()
+            }
+        )
     }
 }
+
 @Composable
-fun DetailStatus(
-    mhsUiState: DetailMahasiswaUiState,
-    retryAction: () -> Unit,
+fun DetailBody(
+    detailUiState: DetailUiState,
     modifier: Modifier = Modifier,
+    onDeleteClick: () -> Unit
 ) {
-    when (mhsUiState) {
-        is DetailMahasiswaUiState.Success -> {
-            DetailCard(
-                mahasiswa = mhsUiState.mahasiswa,
-                modifier = modifier.padding(16.dp)
-            )
-        }
+    var deleteConfirmationRequired by remember { mutableStateOf(false) }
 
-        is DetailMahasiswaUiState.Loading -> {
-            Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                CircularProgressIndicator()
+    when (detailUiState) {
+        is DetailUiState.Loading -> OnLoading(modifier = modifier)
+        is DetailUiState.Success -> Column(
+            modifier = modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            horizontalAlignment = Alignment.Start
+        ) {
+            DetailContent(mahasiswa = detailUiState.mahasiswa)
+            Button(
+                onClick = { deleteConfirmationRequired = true },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "Delete")
             }
         }
+        is DetailUiState.Error -> OnError(retryAction = {}, modifier = modifier)
+    }
 
-        is DetailMahasiswaUiState.Error -> {
-            Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    Text(text = "Terjadi kesalahan.")
-                    Spacer(modifier = Modifier.height(8.dp))
-                    Button(onClick = retryAction) {
-                        Text(text = "Coba Lagi")
-                    }
-                }
-            }
-        }
+    if (deleteConfirmationRequired) {
+        DeleteConfirmationDialog(
+            onDeleteConfirm = {
+                deleteConfirmationRequired = false
+                onDeleteClick()
+            },
+            onDeleteCancel = { deleteConfirmationRequired = false }
+        )
     }
 }
 
 @Composable
-fun DetailCard(
-    mahasiswa: Mahasiswa,
-    modifier: Modifier = Modifier
-) {
+fun DetailContent(mahasiswa: Mahasiswa, modifier: Modifier = Modifier) {
     Card(
-        modifier = modifier,
-        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
-        shape = MaterialTheme.shapes.medium
+        modifier = modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            contentColor = Color.White,
+            containerColor = MaterialTheme.colorScheme.primary
+        )
     ) {
-        Column(
-            modifier = Modifier.padding(16.dp),
-            //horizontalAlignment = Alignment.Start,
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
+        Column(modifier = Modifier.padding(16.dp)) {
             ComponentDetailMhs(judul = "NIM", isinya = mahasiswa.nim)
-            Spacer(modifier = Modifier.height(8.dp))
+            Spacer(modifier = Modifier.padding(8.dp))
             ComponentDetailMhs(judul = "Nama", isinya = mahasiswa.nama)
-            Spacer(modifier = Modifier.height(8.dp))
+            Spacer(modifier = Modifier.padding(8.dp))
             ComponentDetailMhs(judul = "Alamat", isinya = mahasiswa.alamat)
-            Spacer(modifier = Modifier.height(8.dp))
+            Spacer(modifier = Modifier.padding(8.dp))
             ComponentDetailMhs(judul = "Jenis Kelamin", isinya = mahasiswa.jenisKelamin)
-            Spacer(modifier = Modifier.height(8.dp))
+            Spacer(modifier = Modifier.padding(8.dp))
             ComponentDetailMhs(judul = "Kelas", isinya = mahasiswa.kelas)
-            Spacer(modifier = Modifier.height(8.dp))
+            Spacer(modifier = Modifier.padding(8.dp))
             ComponentDetailMhs(judul = "Angkatan", isinya = mahasiswa.angkatan)
         }
     }
 }
 
 @Composable
-fun ComponentDetailMhs(
-    modifier: Modifier = Modifier,
-    judul: String,
-    isinya: String,
-) {
+fun ComponentDetailMhs(judul: String, isinya: String, modifier: Modifier = Modifier) {
     Row(
-        modifier = modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(4.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
-            text = "$judul:",
+            text = judul,
+            Modifier.weight(0.8f),
             fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.Gray
+            color = Color.LightGray
+        )
+        Text(
+            text = ":",
+            Modifier.weight(0.2f),
+            fontSize = 20.sp,
+            color = Color.LightGray
         )
         Text(
             text = isinya,
+            Modifier.weight(2f),
             fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
+            color = Color.White
         )
     }
+}
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(
+        onDismissRequest = { },
+        title = { Text("Delete Data") },
+        text = { Text("Apakah anda yakin ingin menghapus data?") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Yes")
+            }
+        }
+    )
 }
